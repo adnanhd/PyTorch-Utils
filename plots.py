88c869b083ee9186c469c3fbcacfaaa1265c8d21
adnanhd <adnanharundogan@gmail.com>
2021-11-20 14:27:02 +0300

    Change plot parameters trainer -> plot

--- plots.py
+++ plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def subplot_train(trainer, train_loss, valid_loss, **kwargs):
+def subplot_train(path, train_loss, valid_loss, **kwargs):
     fig, ax = subplots()
     epochs = len(train_loss)
     ax.set_title('Training and Validation Loss in {} Iter'.format(epochs))
@@ -11,17 +11,17 @@ def subplot_train(trainer, train_loss, valid_loss, **kwargs):
     ax.semilogy(range(epochs), train_loss, label='train')
     ax.semilogy(range(epochs), valid_loss, label='valid')
     ax.legend()
-    plt.savefig(os.path.join(trainer.plot_path, "train_loss_logy_{}_iter.png".format(epochs)))
+    plt.savefig(os.path.join(path, "train_loss_logy_{}_iter.png".format(epochs)))
     plt.close(fig)
 
 
-def subplot_test(trainer, epochs, test_loss, **kwargs):
+def subplot_test(path, epochs, test_loss, **kwargs):
     fig, ax = plt.subplots()
     ax.set_title('Testing Loss in {} Case'.format(len(test_loss)))
     ax.set_ylabel('Cases')
     ax.set_xlabel('Loss (avg. {:.3e})'.format(torch.as_tensor(test_loss).mean()))
     ax.hist(test_loss, bins=int(math.log2(len(test_loss) ** 2)))
-    plt.savefig(os.path.join(trainer.plot_path, 'test_loss_hist_{}_iter.png'.format(epochs)))
+    plt.savefig(os.path.join(path, 'test_loss_hist_{}_iter.png'.format(epochs)))
     plt.close(fig)
 
 
