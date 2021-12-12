7e3184825dcdc75de8396d07dae1d3846bb9fe2c
adnanhd <adnanharundogan@gmail.com>
2021-12-13 01:00:00 +0300

    Add new callbacks

--- callbacks.py
+++ callbacks.py
@@ -1,9 +1,24 @@
 import numpy as np
 import torch
 
-class EarlyStopping:
+
+class Callback:
+    def __init__(self):
+        pass
+
+    def on_training_end(self, trainer, **kwargs):
+        pass
+
+    def on_testing_end(self, trainer, **kwargs):
+        pass
+
+    def on_epoch_end(self, trainer, **kwargs):
+        pass
+
+
+class EarlyStopping(Callback):
     """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+    def __init__(self, patience=7, verbose=False, delta=0, trace_func=print):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
@@ -24,20 +39,21 @@ class EarlyStopping:
         self.early_stop = False
         self.val_loss = np.Inf
         self.delta = delta
-        self.path = path
         self.trace_func = trace_func
-    def __call__(self, trainer, valid_loss=None, epoch=None, **kwargs):
+
+    def on_epoch_end(self, trainer, valid_loss=None, epoch=None, **kwargs):
         score = -valid_loss
 
         if self.best_score is None:
             self.best_score = score
         elif score < self.best_score + self.delta:
             self.counter += 1
-            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.verbose:
+                self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
                 trainer.stop_iter(restart=False)
-                trainer.save_checkpoint(epoch=epoch, path=self.path)
+                trainer.save_checkpoint(epoch=epoch)
         else:
             self.best_score = score
             self.counter = 0
@@ -45,20 +61,24 @@ class EarlyStopping:
         return self.early_stop
 
 
-class WandbLogger:
-    def __init__(self, project=None, entity=None, model=None, **config):
-        self.wandb = wandb.init(project=project, entity=entity, config=config)
-        self.plots = dict()
-        
-        if model is not None:
-            self.wandb.watch(self.model, log="all")
+class SaveBestModel(Callback):
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, save_best_metric='valid_loss', verbose=False, this_max=False):
+        self.save_best_metric = save_best_metric
+        self.best_weights = None
+        self.max = this_max
+        self.verbose = verbose
+        self.best = float('-inf') if this_max else flot('inf')
 
+    def on_epoch_end(self, trainer, epoch=None, **kwargs):
+        metric_value = kwargs[self.save_best_metric]
 
-    def plot(self, key, value):
-        self.plots[key] = value
+        if self.max and metric_value > self.best or metric_value < self.best:
+            self.best = metric_value
+            self.best_weights = trainer.model.state_dict()
+            if self.verbose: 
+                print("best model is saved...")
 
-    def step(self, epoch):
-        pass
-
-    def __call__(self, trainer, epoch=None, **kwargs):
-        pass
+    def on_train_end(self, trainer, **kwargs):
+        if self.best_weights is not None:
+            self.model.save_state_dict(self.best_weights)
