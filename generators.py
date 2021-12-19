b84ade83b26391c356718fcd45380b06bc82d70d
adnanhd <adnanharundogan@gmail.com>
2021-12-19 18:24:56 +0300

    Update HTMLGenerator

--- generators.py
+++ generators.py
@@ -1,9 +1,11 @@
-import math, os
+import math
+import os
 import datetime
 import torch
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 
 def makedirs(path):
     try:
@@ -11,8 +13,10 @@ def makedirs(path):
     except FileExistsError:
         pass
 
+
 class Generator(object):
     parent_dir = './'
+
     def __init__(self, parent=None, folder=None):
         if parent is None:
             parent = self.parent_dir
@@ -27,24 +31,21 @@ class Generator(object):
 
 
 class FileGenerator(Generator):
+    ending = None
     def __init__(self, buf, parent=None, folder=None, hierarchy=False, preserve=False):
         assert isinstance(buf, str)
         super(FileGenerator, self).__init__(parent=parent, folder=folder)
-        if preserve:
-            fname = buf.split(".")
-            fend = fname.pop(-1)
-            fname = ".".join(fname)
-            buf = f"{fname}.{fend}"
-            i = 0
-            while os.path.isfile(buf):
-                buf = f"{fname}{i}.{fend}"
-                i = i + 1
         self.fname = buf
-        self._buffer = open(os.path.join(self.path, buf), 'w')
+
+        self._buffer = open(os.path.join(self.path, self.fname), 'w')
         self.hierarchy = hierarchy
-    
+        self.add_header()
+
     def __del__(self):
         self._buffer.close()
+
+    def add_header(self):
+        return NotImplementedError()
 
     def add_parent_page(self):
         return NotImplementedError()
@@ -71,28 +72,31 @@ class FileGenerator(Generator):
 class WandbGenerator(Generator):
     def __init__(self, project=None, entity=None, model=None, **config):
         self.wandb = wandb.init(project=project, entity=entity, config=config)
-        
+
         if model is not None:
             self.wandb.watch(self.model, log="all")
 
     def __call__(self, train_df=None, valid_df=None, test_df=None):
         pass
 
+
 class LatexGenerator(FileGenerator):
     main_page = 'main.tex'
     ending = ".tex"
+
     def __init__(self, entity, project=None, model=None, **config):
         if not entity.endswith(self.ending):
             entity = entity + self.ending
 
         self.fname = entity.split(self.ending)[0]
 
-        super(LatexGenerator, self).__init__(entity, project=path, folder='latex')
-        
+        super(LatexGenerator, self).__init__(
+            entity, project=project, folder='latex')
+
         self._buffer.write(r'\documentclass{article}')
         self._buffer.write(r'\usepackage[utf8]{inputenc}')
         self._buffer.write(r'\usepackage{booktabs}')
-        
+
         self._buffer.write(r'\title{' + str(project) + '}')
         self._buffer.write(r'\author{' + str(entity) + '}')
         self._buffer.write(r'\date{December 2021}')
@@ -114,63 +118,66 @@ class LatexGenerator(FileGenerator):
             df.to_html(self._buffer)
 
     def add_image(self, path):
-       self._buffer.write(r"\begin{figure}\includegraphics[width=\linewidth]{" + path \
-                + r"}\caption{A boat.}\label{fig:boat1}\end{figure}")
+        self._buffer.write(r"\begin{figure}\includegraphics[width=\linewidth]{" + path
+                           + r"}\caption{A boat.}\label{fig:boat1}\end{figure}")
 
 
 class HTMLGenerator(FileGenerator):
     main_page = 'index.html'
     ending = ".html"
-    from dominate import tags
+    from dominate import document, tags
 
-    def __init__(self, entity, project=None, model=None, create_main_page=False, overwrite=True, meta={}, **config):
+    def __init__(self, entity, project=None, model=None, 
+                    main_page=False, overwrite=True, **config):
         if not entity.endswith(self.ending):
             entity = entity + self.ending
 
-        super(HTMLGenerator, self).__init__(entity, parent=project, folder='html', hierarchy=create_main_page, preserve=not overwrite)
-        
-        _meta = {"content":"utf-8", "http-equiv":"encoding"}
-        _meta.update(meta)
+        self.doc = self.document(title=entity.split(self.ending)[0])
+        super(HTMLGenerator, self).__init__(entity, parent=project, folder='html', 
+                                            hierarchy=main_page, preserve=not overwrite)
+        print(self.fname, self.ending, entity, entity.split(self.ending)[0])
 
-        self._buffer.write("<!DOCTYPE html>")
-        self._buffer.write(str(self.tags.meta(**_meta)))
-        self.fname = entity.split(self.ending)[0]
         self.basecontent = config
 
     def __del__(self):
-        super(HTMLGenerator, self).__del__()
+        self._buffer.write(str(self.render()))
+
+    def render(self, *args, **kwargs):
+        return self.doc.render(*args, **kwargs)
+    
+    def add_header(self, meta = {"content": "utf-8", "http-equiv": "encoding"}):
+        self.doc.head.add(self.tags.meta(**meta))
+        print(self.hierarchy)
+        if self.hierarchy:
+            self.doc.body.add(self.tags.a("back", href=self.main_page))
+            self.add_parent_page()
 
     def add_parent_page(self):
         with open(os.path.join(self.path, self.main_page), 'w') as f:
-            html_files = list(filter(lambda x: x.endswith(self.ending), os.listdir(self.path)))
+            html_files = list(filter(lambda x: x.endswith(
+                self.ending), os.listdir(self.path)))
             print(html_files)
             if self.main_page in html_files:
                 html_files.remove(self.main_page)
             for each in html_files:
                 fname = each.split(self.ending)[0]
-                f.write(str(self.tags.a(fname, href=fname + self.ending)) + str(self.tags.br()))
+                f.write(str(self.tags.a(fname, href=fname +
+                                        self.ending)) + str(self.tags.br()))
 
     def add_dataframe(self, *dfs):
-        if self.hierarchy:
-            self._buffer.write(str(self.tags.a('back', href=self.main_page)) + str(self.tags.br()))
-        
         for df in dfs:
-            df.to_html(self._buffer)
+            self.doc.body.add_raw_string(df.to_html())
 
     def add_figure(self, fig, fname=None):
         if fname is None:
-            fname = self.fname + fig.ending
+            self_fname = self.fname.split(self.ending)[0]
+            fname = self_fname + fig.ending
             path = os.path.join(self.path, fname)
-            i = 0
-            while os.path.isfile(path):
-                fname = self.fname + str(i) + fig.ending
-                path = os.path.join(self.path, fname)
-                i += 1
         fig.plot(path=path)
-        self._buffer.write(str(self.tags.img(src=fname)))
+        self.doc.body.add(self.tags.img(src=fname))
 
     def add_module(self, model: torch.nn.Module):
-        self._buffer.write(str(self.tags.pre(str(model))))
+        self.doc.body.add(self.tags.pre(str(model)))
 
 
 class _AxisGenerator(Generator):
@@ -193,16 +200,17 @@ class _AxisGenerator(Generator):
 
 class FigureGenerator(Generator):
     ending = '.png'
+
     def __init__(self, *axes, fig=None, project=None, **kwargs):
         super(FigureGenerator, self).__init__(parent=project, folder='plot')
-        
+
         if fig is None:
             self.fig = plt.figure()
 
         self.axes = self.fig.subplots(*axes, **kwargs)
-        
+
         if isinstance(self.axes, plt.Axes):
-            self.axes= np.array([self.axes])
+            self.axes = np.array([self.axes])
 
     def __getitem__(self, index):
         return self.axes[index]
@@ -229,39 +237,42 @@ class FigureGenerator(Generator):
     class Semilogy(_AxisGenerator):
         def __init__(self, df, title=None, xlabel=None, ylabel=None):
             self.df = df
-            
-            if title is None:
-                title=f'Training and Validation Metrics in {len(df)} Iter'
-            if xlabel is None:
-                xlabel='Epochs'
-            if ylabel is None:
-                ylabel='Loss'
 
-            super(self.__class__, self).__init__(title=title, ylabel=ylabel, xlabel=xlabel)
-            
+            if title is None:
+                title = f'Training and Validation Metrics in {len(df)} Iter'
+            if xlabel is None:
+                xlabel = 'Epochs'
+            if ylabel is None:
+                ylabel = 'Loss'
+
+            super(self.__class__, self).__init__(
+                title=title, ylabel=ylabel, xlabel=xlabel)
+
         def __call__(self, ax):
             # TODO: add 'x' and 'o' for train and valid
             super(self.__class__, self).__call__(ax=ax)
             ax.semilogy(self.df, 'o-')
             ax.legend(self.df.columns)
             return ax
-    
+
     class Histogram(_AxisGenerator):
         def __init__(self, df, ax=None, title=None, xlabel=None, ylabel=None):
             self.df = df
-            
-            if title is None:
-                title=f'Testing Metrics in {len(df)} Iter'
-            if xlabel is None:
-                xlabel='Metrics'
-            if ylabel is None:
-                ylabel='Cases'
 
-            super(self.__class__, self).__init__(title=title, ylabel=ylabel, xlabel=xlabel)
+            if title is None:
+                title = f'Testing Metrics in {len(df)} Iter'
+            if xlabel is None:
+                xlabel = 'Metrics'
+            if ylabel is None:
+                ylabel = 'Cases'
+
+            super(self.__class__, self).__init__(
+                title=title, ylabel=ylabel, xlabel=xlabel)
 
         def __call__(self, ax):
             # TODO: add 'x' and 'o' for train and valid
-            super(Histogram, self).__call__(ax=ax)
-            ax.hist(self.df, bins=int(math.log2(len(self.df) ** 2)) if len(self.df) else None)
+            super(self.__class__, self).__call__(ax=ax)
+            ax.hist(self.df, bins=int(math.log2(len(self.df) ** 2) + 1)
+                    if len(self.df) else None)
             ax.legend(self.df.columns)
             return ax
