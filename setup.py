a66f5a7e98d89b71d608dd342137b2e140bc9f48
adnanhd <adnanharundogan@gmail.com>
2022-02-27 02:21:52 +0300

    Convert module to be installed by pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='PyTorch-Utils',
+        version='1.0',
+        description='Basic tools for pytorch needed for training',
+        author='Adnan Harun Dogan',
+        author_email='adnan.dogan@metu.edu.tr',
+        packages=['torchutils'],
+        #install_requires=['torch', 'tqdm', 'numpy', 'pandas']
+)
